8b0e120c79788f44afd629808fb736270375f998
YMB <d2ms2nk@gmail.com>
2019-03-08 22:18:49 +0300

    new files

--- integrals.py
+++ integrals.py
@@ -1,90 +1,90 @@
 ############################
-#                          #
-#      2019 IU7 BMSTU      #
-#                          #
+#						   #
+#	  2019 IU7 BMSTU	   # 
+#						   #
 #  All integrals which can #
-#    be asked on eaxams.   #
-#                          #
-#    Special thanks to:    #
-#      @maxim_kozlov       #
-#                          #
+#	be asked on eaxams.    #
+#						   #
+#	Special thanks to:	   # 
+#	  @maxim_kozlov	       #
+#						   #
 # Written in Pascal style  #
-#                          #
+#						   #
 ############################
 
 def threeEights(start, stop, splits, f):
-    a = start
-    b = stop
-    m = splits
-    h = (b - a) / (m)
-    s = f(a) + f(b)
+	a = start
+	b = stop
+	m = splits
+	h = (b - a) / (m)
+	s = f(a) + f(b)
 
-    for i in range(1, m):
-        x = a + h * i
-        if i % 3 == 0:
-            s = s + 2 * f(x)
-        else:
-            s = s + 3 * f(x)
-    s = 3 * s * h / 8
-    return s
+	for i in range(1, m):
+		x = a + h * i
+		if i % 3 == 0:
+			s = s + 2 * f(x)
+		else:
+			s = s + 3 * f(x)
+	s = 3 * s * h / 8
+	return s
 
 
 def leftRectangles(start, stop, f, splits):
-    a = start
-    b = stop
-    n = splits
-    h = (b - a) / n
-    s = 0
-    for i in range(n):
-        x = a + h * i
-        s += f(x)
-    s *= h
-    return s
+	a = start
+	b = stop
+	n = splits
+	h = (b - a) / n
+	s = 0
+	for i in range(n):
+		x = a + h * i
+		s += f(x)
+	s *= h
+	return s
 
 
 def middleRectangles(start, stop, f, splits):
-    a = start
-    b = stop
-    n = splits
-    h = (b - a) / n
-    s = 0
-    for i in range(n):
-        x = a + h * i + h / 2
-        s += f(x)
-    s *= h
-    return s
+	a = start
+	b = stop
+	n = splits
+	h = (b - a) / n
+	s = 0
+	for i in range(n):
+		x = a + h * i + h / 2
+		s += f(x)
+	s *= h
+	return s
 
 
 def rightRectangles(start, stop, f, splits):
-    a = start
-    b = stop
-    n = splits
-    h = (b - a) / n
-    s = 0
-    for i in range(1, n + 1):
-        x = a + h * i
-        s += f(x)
-    s *= h
-    return s
+	a = start
+	b = stop
+	n = splits
+	h = (b - a) / n
+	s = 0
+	for i in range(1, n + 1):
+		x = a + h * i
+		s += f(x)
+	s *= h
+	return s
 
 
 def sympson(razb, a, b):
-    h = (b - a) / razb
-    s = 0
-    x = a + h
-    while x < b:
-        s = s + 4 * f(x)
-        x = x + h
-        s = s + 2 * f(x)
-        x = x + h
-    Sympson = h / 3 * (s + f(a) - f(b))
-    return Sympson
+	h = (b - a) / razb
+	s = 0
+	x = a + h
+	while x < b:
+		s = s + 4 * f(x)
+		x = x + h
+		s = s + 2 * f(x)
+		x = x + h
+	Sympson = h / 3 * (s + f(a) - f(b))
+	return Sympson
 
 
 def trapezum(razb, func=f, a, b):
-    h = (b - a) / (razb)
-    result = 0.5 * (f(a) + f(b))
-    for i in range(1, razb):
-        result += f(a + i * h)
-    result *= h
-    return result
+	h = (b - a) / (razb)
+	result = 0.5 * (f(a) + f(b))
+	for i in range(1, razb):
+		result += f(a + i * h)
+	result *= h
+	return result
--- roots.py
+++ roots.py
@@ -1,85 +1,84 @@
 ############################
-#                          #
-#      2019 IU7 BMSTU      #
-#                          #
-#      All methods of      #
+#						   #
+#	  2019 IU7 BMSTU	   #
+#						   #
+#	  All methods of	   #
 #  root refinement which   #
 #  can be asked on eaxams  #
-#                          #
-#    Special thanks to:    #
-#      @maxim_kozlov       #
-#                          #
+#						   #
+#	Special thanks to:	   #
+#	  @maxim_kozlov	       #
+#						   # 
 # Written in Pascal style  #
-#                          #
+#						   #
 ############################
 
 def chords(x0, b, f, eps):
-    x0 = x1
-    fb = f(b)
-    while not (abs(f(x1)) < eps):
-        t = x1
-        x1 = x0 - f(x0) * (b - x0) / (fb - f(x0))
-        x0 = t
-    return x1
+	x0 = x1
+	fb = f(b)
+	while not (abs(f(x1)) < eps):
+		t = x1
+		x1 = x0 - f(x0) * (b - x0) / (fb - f(x0))
+		x0 = t
+	return x1
 
 
 def binary(start, stop, eps, f):
-    middle = (start + stop) / 2
-    while not (abs(f(middle)) < eps):
-        middle = (start + stop) / 2
-        if f(start) * f(m) < 0:
-            stop = middle
-        else:
-            start = middle
-    return middle
+	middle = (start + stop) / 2
+	while not (abs(f(middle)) < eps):
+		middle = (start + stop) / 2
+		if f(start) * f(m) < 0:
+			stop = middle
+		else:
+			start = middle
+	return middle
 
 
 def combinated(x0, x_0, eps, f, df):
-    x0 = x1 = 2
-    x_0 = x_1 = 5
-    eps = 1e-7
-    while not (abs(f(x1)) < eps):
-        x1, x0 = x0 - f(x0) * (x_1 - x0) / (f(x_1) - f(x0)), x1
-        x_1, x_0 = x_0 - f(x_0) / df(x_0), x_1
-    return x1
+	x0 = x1
+	x_0 = x_1
+	while not (abs(f(x1)) < eps):
+		x1, x0 = x0 - f(x0) * (x_1 - x0) / (f(x_1) - f(x0)), x1
+		x_1, x_0 = x_0 - f(x_0) / df(x_0), x_1
+	return x1
 
 
 def steffenson(x0, eps, f):
-    x0 = x1
-    while not (abs(f(x1)) < eps):
-        t = x1
-        f0 = f(x0)
-        x1 = x0 - f0 * f0 / (f(x0 + f0) - f0)
-        x0 = t
-    return x1
+	x0 = x1
+	while not (abs(f(x1)) < eps):
+		t = x1
+		f0 = f(x0)
+		x1 = x0 - f0 * f0 / (f(x0 + f0) - f0)
+		x0 = t
+	return x1
 
 
 def sipleNewton(x0, eps, f, df):
-    x0 = x1
-    df_x0 = df(x0)
-    while not (abs(f(x1)) < eps):
-        t = x1
-        x1 = x0 - f(x0) / df_x0
-        x0 = t
-    return x1
+	x0 = x1
+	df_x0 = df(x0)
+	while not (abs(f(x1)) < eps):
+		t = x1
+		x1 = x0 - f(x0) / df_x0
+		x0 = t
+	return x1
 
 
 def secants(x0, x1, eps, f):
-    x2 = x0
-    while not (abs(f(x2)) < eps):
-        x2, x1, x0 = x1 - f(x1) * (x1 - x0) / (f(x1) - f(x0)), x2, x1
-    return x2
+	x2 = x0
+	while not (abs(f(x2)) < eps):
+		x2, x1, x0 = x1 - f(x1) * (x1 - x0) / (f(x1) - f(x0)), x2, x1
+	return x2
 
 
 def newton(f, df, eps, x0):
-    x0 = x1
-    while not (abs(f(x1)) < eps):
-        x1, x0 = x0 - f(x0) / df(x0), x1
-    return x1
+	x0 = x1
+	while not (abs(f(x1)) < eps):
+		x1, x0 = x0 - f(x0) / df(x0), x1
+	return x1
 
 
 def iterations(eps, x0, f, fi):
-    x1 = x0
-    while not (abs(f(x1)) < eps):
-        x1, x0 = fi(x0), x1
-    return x1
+	x1 = x0
+	while not (abs(f(x1)) < eps):
+		x1, x0 = fi(x0), x1
+	return x1
